18be5b52a0ab4876b7481bbedbbc894301e50fbb
johnnylyimo <johnnylyimo@gmail.com>
2019-12-01 16:42:59 +0300

    Removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'hello_rectangle',
       theme: ThemeData(
         primaryColor: Colors.blueGrey.shade500,

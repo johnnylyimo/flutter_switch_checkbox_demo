c010d5847b82a93de0e15d39a12f9a388afc9523
johnnylyimo <johnnylyimo@gmail.com>
2019-12-04 01:03:30 +0300

    Added Switch and Checkbox Widgets

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Color colorType = Colors.yellow;
+  bool isSwitched = false;
+  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,10 +22,68 @@ class MyHomePage extends StatelessWidget {
           color: Colors.blueGrey.shade700,
           width: 220.0,
           height: 180.0,
-          child: Center(
-            child: Center(
-              child: Text('Hello Folks'),
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Text(
+                  'Hello Folks',
+                  style: TextStyle(color: colorType),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('BlueColor'),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Switch(
+                    value: isSwitched,
+                    // store user tap to toggle
+                    onChanged: (bool toggled) {
+                      setState(() {
+                        isSwitched = toggled; // update isSwitched to toggled
+                        isSwitched
+                            ? colorType = Colors.blue
+                            : colorType = Colors.yellow;
+                      });
+                    },
+                    activeTrackColor: Colors.blue.shade300,
+                    activeColor: Colors.blue.shade500,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'GreenColor',
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Checkbox(
+                    value: isChecked,
+                    // store user tap checked
+                    onChanged: (bool checked) {
+                      setState(() {
+                        isChecked = checked; // updated isChecked to checked
+                        isChecked
+                            ? colorType = Colors.green
+                            : colorType = Colors.red;
+                      });
+                    },
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
